4698a782b4c86c3d7ceb882bf1fe910cb9a34cc4
JasonGogo19 <jason.gogo@ashesi.edu.gh>
2024-04-12 21:49:34 +0000

    fixed css

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>J Event's</title>
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body> 
 
