c064cf489cd05411f5e50764843165e5433ea8f0
JasonGogo19 <143032547+JasonGogo19@users.noreply.github.com>
2024-04-13 12:25:16 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="'viewport" content="width_device-width, initial-scale">
+    <meta name="viewport" content="width_device-width, initial-scale=1.0">
     <title>J Event's</title>
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
@@ -21,8 +21,8 @@
                 <li><a href="#work">Work</a></li>
                 <li><a href="#blog">Blog</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li><a href="../final_project/login/login.php">Log in</a></li>
-                <li><a href="../final_project/login/register.php">Sign up</a></li>
+                <li><a href="../login/login.php">Log in</a></li>
+                <li><a href="../login/register.php">Sign up</a></li>
             </ul>
             <div class="hamburger">
                 <span></span>
